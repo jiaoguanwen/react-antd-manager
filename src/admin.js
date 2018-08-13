ebad30f92ed30ad7a4b98dd6c2cf719e17fa3e6c
jiaoguanwen <jiaoguanwen@163.com>
2018-08-13 19:00:10 +0800

    go on 3-6

--- src/admin.js
+++ src/admin.js
@@ -2,25 +2,24 @@ import React from 'react'
 import { Row, Col } from 'antd'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+import './style/common.less'
 
 export default class Admin extends React.Component {
 
   render() {
     return (
-      <Row>
-        <Col span="3">
-          LEFT
+      <Row className="container">
+        <Col span="3" className="nav-left">
+          <NavLeft />
         </Col>
-        <Col span="21">
-          <Header>
-            Header
-          </Header>
-          <Row>
+        <Col span="21" className="main">
+          <Header />
+          <Row className="content">
             Content
+            {/* {this.props.children} */}
           </Row>
-          <Footer>
-            Footer
-          </Footer>
+          <Footer />
         </Col>
       </Row>
     )
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import MenuConfig from '../../config/menuConfig'
+import { Menu, Icon } from 'antd'
+import './index.less'
+
+const SubMenu = Menu.SubMenu
+
+export default class NavLeft extends React.Component {
+  render() {
+    /* let style = {
+      backgroundColor: 'red'
+    } */
+    return (
+      // <div style={style}>
+      <div>
+        {/* <a href="">NavLeft</a> */}
+        <div className="logo">
+          <img src="/assets/logo-ant.svg" alt="" />
+          <h1>Access</h1>
+        </div>
+        <Menu theme="dark">
+          <SubMenu key="sub1" title="Menu">
+            <Menu.Item key="1">1</Menu.Item>
+            <Menu.Item key="2">2</Menu.Item>
+            <Menu.Item key="3">3</Menu.Item>
+            <Menu.Item key="4">4</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </div>
+    )
+  }
+}
