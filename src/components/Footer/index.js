930d2d02258af658ae694a6041e34a0353b70b04
jiaoguanwen <jiaoguanwen@csii.com.cn>
2018-08-14 00:33:03 +0800

    add Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import './index.less'
 
 export default class Footer extends React.Component {
   render() {
     return (
-      <div>
-        Footer
-      </div>
+      <div className="footer">版权所有: 焦冠文(推荐使用谷歌浏览器，可以获得更佳操作页面体验) 技术支持: 焦冠文</div>
     )
   }
 }
