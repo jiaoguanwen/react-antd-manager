3067e627e31119ce3d199f93c34a73be08e2449c
jiaoguanwen <jiaoguanwen@163.com>
2018-10-19 17:38:09 +0800

    add carousel gallery

--- src/router.js
+++ src/router.js
@@ -9,6 +9,8 @@ import Loadings from './pages/ui/loadings'
 import Notices from './pages/ui/notices'
 import Messages from './pages/ui/messages'
 import ITabs from './pages/ui/i-tabs'
+import Gallery from './pages/ui/gallery'
+import Carousel from './pages/ui/carousel'
 import NotMatch from './pages/notMatch'
 
 export default class IRouter extends React.Component {
@@ -17,19 +19,24 @@ export default class IRouter extends React.Component {
       <HashRouter>
         <App>
           <Route path="/login" component={Login} />
-          <Route path="/admin" render={() =>
-            <Admin>
-              <Switch>
-                <Route path="/admin/ui/buttons" component={Buttons} />
-                <Route path="/admin/ui/modals" component={Modals} />
-                <Route path="/admin/ui/loadings" component={Loadings} />
-                <Route path="/admin/ui/notices" component={Notices} />
-                <Route path="/admin/ui/messages" component={Messages} />
-                <Route path="/admin/ui/tabs" component={ITabs} />
-                <Route component={NotMatch} />
-              </Switch>
-            </Admin>
-          } />
+          <Route
+            path="/admin"
+            render={() => (
+              <Admin>
+                <Switch>
+                  <Route path="/admin/ui/buttons" component={Buttons} />
+                  <Route path="/admin/ui/modals" component={Modals} />
+                  <Route path="/admin/ui/loadings" component={Loadings} />
+                  <Route path="/admin/ui/notices" component={Notices} />
+                  <Route path="/admin/ui/messages" component={Messages} />
+                  <Route path="/admin/ui/tabs" component={ITabs} />
+                  <Route path="/admin/ui/gallery" component={Gallery} />
+                  <Route path="/admin/ui/carousel" component={Carousel} />
+                  <Route component={NotMatch} />
+                </Switch>
+              </Admin>
+            )}
+          />
           <Route path="/order/detail" component={Login} />
         </App>
       </HashRouter>
