eaf0d574246a7ee546849a86dbc1f5d519cc2a51
jiaoguanwen <jiaoguanwen@163.com>
2018-10-21 22:05:47 +0800

    complete register

--- src/pages/form/register.js
+++ src/pages/form/register.js
@@ -1,5 +1,21 @@
 import React from 'react'
-import { Card, Form, Button, Input, Checkbox, Radio, Select, Switch, DatePicker, TimePicker, upload, Icon, message, InputNumber, Upload } from 'antd'
+import {
+  Card,
+  Form,
+  Button,
+  Input,
+  Checkbox,
+  Radio,
+  Select,
+  Switch,
+  DatePicker,
+  TimePicker,
+  upload,
+  Icon,
+  message,
+  InputNumber,
+  Upload
+} from 'antd'
 import moment from 'moment'
 
 const FromItem = Form.Item
@@ -8,8 +24,39 @@ const Option = Select.Option
 const TextArea = Input.TextArea
 
 class FromRegister extends React.Component {
-
   state = {}
+
+  handleSubmit = () => {
+    let userInfo = this.props.form.getFieldsValue()
+    console.log(userInfo)
+    message.success(
+      `${userInfo.userName} 恭喜你，您通过本次表单组件学习，当前密码为：${
+        userInfo.userPwd
+      }`
+    )
+  }
+
+  handleChange = info => {
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true })
+      return
+    }
+    if (info.file.status === 'done') {
+      // get this url from respose in real world
+      this.getBase64(info.file.originFileObj, imageUrl =>
+        this.setState({
+          userImg: imageUrl,
+          loading: false
+        })
+      )
+    }
+  }
+
+  getBase64 = (img, callback) => {
+    const reader = new FileReader()
+    reader.addEventListener('load', () => callback(reader.result))
+    reader.readAsDataURL(img)
+  }
 
   render() {
     const { getFieldDecorator } = this.props.form
@@ -41,134 +88,112 @@ class FromRegister extends React.Component {
         <Card title="注册表单">
           <Form layout="horizontal">
             <FromItem label="用户名" {...formItemLayout}>
-              {
-                getFieldDecorator('userName', {
-                  initialValue: '',
-                  rules: [
-                    {
-                      required: true,
-                      message: '用户名不能为空'
-                    }
-                  ]
-                })(
-                  <Input placeholder="请输入用户名" />
-                )
-              }
+              {getFieldDecorator('userName', {
+                initialValue: '',
+                rules: [
+                  {
+                    required: true,
+                    message: '用户名不能为空'
+                  }
+                ]
+              })(<Input placeholder="请输入用户名" />)}
             </FromItem>
             <FromItem label="密码" {...formItemLayout}>
-              {
-                getFieldDecorator('userPwd', {
-                  initialValue: ''
-                })(
-                  <Input type="password" placeholder="请输入密码" />
-                )
-              }
+              {getFieldDecorator('userPwd', {
+                initialValue: ''
+              })(<Input type="password" placeholder="请输入密码" />)}
             </FromItem>
             <FromItem label="性别" {...formItemLayout}>
-              {
-                getFieldDecorator('sex', {
-                  initialValue: '1'
-                })(
-                  <RadioGroup>
-                    <Radio value="1">男</Radio>
-                    <Radio value="2">女</Radio>
-                  </RadioGroup>
-                )
-              }
+              {getFieldDecorator('sex', {
+                initialValue: '1'
+              })(
+                <RadioGroup>
+                  <Radio value="1">男</Radio>
+                  <Radio value="2">女</Radio>
+                </RadioGroup>
+              )}
             </FromItem>
             <FromItem label="年龄" {...formItemLayout}>
-              {
-                getFieldDecorator('age', {
-                  initialValue: 18
-                })(
-                  <InputNumber />
-                )
-              }
+              {getFieldDecorator('age', {
+                initialValue: 18
+              })(<InputNumber />)}
             </FromItem>
             <FromItem label="当前状态" {...formItemLayout}>
-              {
-                getFieldDecorator('state', {
-                  initialValue: '2'
-                })(
-                  <Select>
-                    <Option value="1">咸鱼一条</Option>
-                    <Option value="2">风华浪子</Option>
-                    <Option value="3">北大才子一枚</Option>
-                    <Option value="4">百度FE</Option>
-                    <Option value="5">创业者</Option>
-                  </Select>
-                )
-              }
+              {getFieldDecorator('state', {
+                initialValue: '2'
+              })(
+                <Select>
+                  <Option value="1">咸鱼一条</Option>
+                  <Option value="2">风华浪子</Option>
+                  <Option value="3">北大才子一枚</Option>
+                  <Option value="4">百度FE</Option>
+                  <Option value="5">创业者</Option>
+                </Select>
+              )}
             </FromItem>
             <FromItem label="爱好" {...formItemLayout}>
-              {
-                getFieldDecorator('interest', {
-                  initialValue: ['2', '5']
-                })(
-                  <Select mode="multiple">
-                    <Option value="1">游泳</Option>
-                    <Option value="2">打篮球</Option>
-                    <Option value="3">踢足球</Option>
-                    <Option value="4">跑步</Option>
-                    <Option value="5">爬山</Option>
-                    <Option value="6">骑行</Option>
-                    <Option value="7">桌球</Option>
-                    <Option value="8">麦霸</Option>
-                  </Select>
-                )
-              }
+              {getFieldDecorator('interest', {
+                initialValue: ['2', '5']
+              })(
+                <Select mode="multiple">
+                  <Option value="1">游泳</Option>
+                  <Option value="2">打篮球</Option>
+                  <Option value="3">踢足球</Option>
+                  <Option value="4">跑步</Option>
+                  <Option value="5">爬山</Option>
+                  <Option value="6">骑行</Option>
+                  <Option value="7">桌球</Option>
+                  <Option value="8">麦霸</Option>
+                </Select>
+              )}
             </FromItem>
             <FromItem label="是否已婚" {...formItemLayout}>
-              {
-                getFieldDecorator('isMarried', {
-                  valuePropName: 'checked',
-                  initialValue: true
-                })(
-                  <Switch />
-                )
-              }
+              {getFieldDecorator('isMarried', {
+                valuePropName: 'checked',
+                initialValue: true
+              })(<Switch />)}
             </FromItem>
             <FromItem label="生日" {...formItemLayout}>
-              {
-                getFieldDecorator('birthday', {
-                  initialValue: moment('2018-08-08')
-                })(
-                  <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
-                )
-              }
+              {getFieldDecorator('birthday', {
+                initialValue: moment('2018-08-08')
+              })(<DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />)}
             </FromItem>
             <FromItem label="联系地址" {...formItemLayout}>
-              {
-                getFieldDecorator('address', {
-                  initialValue: '北京市海淀区奥林匹克公园'
-                })(
-                  <TextArea autosize={rowObject} />
-                )
-              }
+              {getFieldDecorator('address', {
+                initialValue: '北京市海淀区奥林匹克公园'
+              })(<TextArea autosize={rowObject} />)}
             </FromItem>
             <FromItem label="早起时间" {...formItemLayout}>
-              {
-                getFieldDecorator('time')(
-                  <TimePicker />
-                )
-              }
+              {getFieldDecorator('time')(<TimePicker />)}
             </FromItem>
             <FromItem label="头像" {...formItemLayout}>
-              {
-                getFieldDecorator('userImg')(
-                  <Upload listType="picture-card" showUploadList={false} action="//jsonplaceholder.typicode.com/posts/" onChange={this.handleChange}>{this.state.userImg ? <img src={this.state.userImg} alt="" /> : <Icon type="plus" />}</Upload>
-                )
-              }
+              {getFieldDecorator('userImg')(
+                <Upload
+                  listType="picture-card"
+                  showUploadList={false}
+                  action="//jsonplaceholder.typicode.com/posts/"
+                  onChange={this.handleChange}
+                >
+                  {this.state.userImg ? (
+                    <img src={this.state.userImg} alt="" />
+                  ) : (
+                    <Icon type="plus" />
+                  )}
+                </Upload>
+              )}
             </FromItem>
             <FromItem {...offsetLayout}>
-              {
-                getFieldDecorator('userImg')(
-                  <Checkbox>我已阅读过<a href="javascript: void(0)">慕课协议</a></Checkbox>
-                )
-              }
+              {getFieldDecorator('userImg')(
+                <Checkbox>
+                  我已阅读过
+                  <a href="javascript: void(0)">慕课协议</a>
+                </Checkbox>
+              )}
             </FromItem>
             <FromItem {...offsetLayout}>
-              <Button type="primary" onClick={this.handleSubmit}>注册</Button>
+              <Button type="primary" onClick={this.handleSubmit}>
+                注册
+              </Button>
             </FromItem>
           </Form>
         </Card>
