5493c78eb47d5b25a220f7a1dd5c3109b181cf98
jiaoguanwen <jiaoguanwen@163.com>
2018-10-23 15:30:26 +0800

    add axios timeout interceptors

--- src/Axios/index.js
+++ src/Axios/index.js
@@ -1,6 +1,34 @@
 import Jsonp from 'jsonp'
 import axios from 'axios'
 import { Modal } from 'antd'
+
+axios.defaults.retry = 4
+axios.defaults.retryDelay = 1000
+axios.interceptors.response.use(undefined, err => {
+  var config = err.config
+  // If config does not exist or the retry option is not set, reject
+  if (!config || !config.retry) return Promise.reject(err)
+  // Set the variable for keeping track of the retry count
+  config.__retryCount = config.__retryCount || 0
+  // Check if we've maxed out the total number of retries
+  if (config.__retryCount >= config.retry) {
+    // Reject with the error
+    return Promise.reject(err)
+  }
+  // Increase the retry count
+  config.__retryCount += 1
+  // Create new promise to handle exponential backoff
+  var backoff = new Promise(function(resolve) {
+    setTimeout(function() {
+      resolve()
+    }, config.retryDelay || 1)
+  })
+  // Return the promise in which recalls axios to retry the request
+  return backoff.then(function() {
+    return axios(config)
+  })
+})
+
 export default class Axios {
   static jsonp(options) {
     return new Promise((resolve, reject) => {
@@ -32,7 +60,7 @@ export default class Axios {
         url: options.url,
         method: 'get',
         baseURL: baseApi,
-        timeout: 50000,
+        timeout: 6000,
         params: (options.data && options.data.params) || ''
       }).then(response => {
         if (options.data && options.data.isShowLoading !== false) {
